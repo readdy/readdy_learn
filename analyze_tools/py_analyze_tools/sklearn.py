f394b37ed2a919c893586202feed9e8d0269cf25
Moritz Hoffmann <clonker@gmail.com>
2017-06-09 15:36:24 +0200

    wip

--- analyze_tools/py_analyze_tools/sklearn.py
+++ analyze_tools/py_analyze_tools/sklearn.py
@@ -260,12 +260,12 @@ class CV(object):
             test_estimator = ReaDDyElasticNetEstimator(self.test_traj, self.bfc, self.scale, alpha=alpha,
                                                        l1_ratio=l1_ratio, init_xi=self.init_xi, verbose=self.verbose,
                                                        method=self.method)
-            test_estimator.coefficients_ = estimator.coefficients_
         scores = []
         for train_idx, test_idx in kf.split(range(0, self.traj.n_time_steps)):
             estimator.fit(train_idx)
             if estimator.result_.success:
                 if self.test_traj is not None:
+                    test_estimator.coefficients_ = estimator.coefficients_
                     scores.append(
                         test_estimator.score(range(0, self.test_traj.n_time_steps), self.test_traj.dcounts_dt))
                 else:
