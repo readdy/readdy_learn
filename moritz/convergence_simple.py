bb6095cdaabae5d2215251236fb9ea0a26cd7fcf
Moritz Hoffmann <clonker@gmail.com>
2017-10-11 13:51:56 +0200

    set up simple convergence of rates example

--- moritz/convergence_simple.py
+++ moritz/convergence_simple.py
@@ -8,13 +8,16 @@ from readdy_learn.analyze.sklearn import ReaDDyElasticNetEstimator
 import matplotlib.pyplot as plt
 
 
-def run(n_steps, n_frames=None, timestep=None):
+def simulate(n_steps):
     system = kmc.ReactionDiffusionSystem(n_species=2, n_boxes=1, diffusivity=[[[0.]], [[0.]]], init_state=[[70, 0]],
                                          species_names=["A", "B"])
     system.add_conversion("A", "B", np.array([4.]))
     system.add_conversion("B", "A", np.array([0.5]))
     system.simulate(n_steps)
+    return system
 
+
+def run(system, n_frames=None, timestep=None):
     counts, times, config = system.get_counts_config(n_frames=n_frames, timestep=timestep)
 
     traj = pat.Trajectory.from_counts(config, counts, times[1] - times[0])
@@ -37,15 +40,21 @@ if __name__ == '__main__':
     if os.path.exists(outfile):
         raise ValueError("File already existed: {}".format(outfile))
 
-    allrates = []
+    allrates = {}
     timesteps = [.000001, .00001, .0001] + [x for x in np.arange(.001, .5, step=.005)]
-    for dt in timesteps:
-        rates = []
-        for n in range(10):
-            rates.append(run(300, timestep=dt))
-            print(rates)
-        rates = np.asarray(rates).squeeze()
-        allrates.append(rates)
+    for k in timesteps:
+        allrates[k] = []
+
+    for n in range(20):
+        system = simulate(300)
+        for dt in timesteps:
+            rates = run(system, timestep=dt)
+            allrates[dt].append(rates)
+    for k in allrates.keys():
+        allrates[k] = np.asarray(allrates[k])
     np.save(outfile, allrates)
-    for rates, dt in zip(allrates, timesteps):
-        print("got {} and {} for timestep={}".format(np.mean(rates[:, 0]), np.mean(rates[:, 1]), dt))
+    for dt in timesteps:
+        rates = allrates[dt]
+        print("got {:.3f}±{:.3f}  and {:.3f}±{:.3f} for timestep={}".format(
+            np.mean(rates[:, 0]), np.std(rates[:, 0]),
+            np.mean(rates[:, 1]), np.std(rates[:, 1]), dt))
