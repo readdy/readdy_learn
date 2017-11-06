c14d1fed79eab739b812641cffdf6d95844028ee
Moritz Hoffmann <clonker@gmail.com>
2017-11-06 12:51:49 +0100

    store derivatives

--- readdy_learn/analyze/derivative.py
+++ readdy_learn/analyze/derivative.py
@@ -212,11 +212,14 @@ def ld_derivative(data, xs, alpha, maxit=1000, linalg_solver_maxit=100, tol=1e-4
     E_n = sparse.dia_matrix((n - 1, n - 1), dtype=xs.dtype)
 
     progress = None
+    label = None
     if show_progress:
-        from ipywidgets import IntProgress
+        from ipywidgets import IntProgress, Label, Box
         from IPython.display import display
         progress = IntProgress(min=0, max=maxit)
-        display(progress)
+        label = Label("Progress: 0/{} it, atol={}/{}, rtol={}/{}".format(0, '?', atol, '?', rtol))
+        box = Box([progress, label])
+        display(box)
 
     outer_v = []
 
@@ -271,6 +274,8 @@ def ld_derivative(data, xs, alpha, maxit=1000, linalg_solver_maxit=100, tol=1e-4
 
         if show_progress:
             progress.value = ii+1
+            label.value = "Progress: {}/{} it, atol={}/{}, rtol={}/{}".format(ii, maxit, np.linalg.norm(g), atol,
+                                                                              relative_change, rtol)
 
         if atol is not None and np.linalg.norm(g) < atol:
             print("ld derivative reached atol = {} < {}, finish".format(atol, np.linalg.norm(g)))
