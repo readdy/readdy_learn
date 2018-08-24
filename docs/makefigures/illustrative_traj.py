2f879bbe328936bd4b1edd3dbb8ad90d13934c03
Christoph Fröhner <christoph.froehner@fu-berlin.de>
2018-08-24 14:39:07 +0200

    wip

--- docs/makefigures/illustrative_traj.py
+++ docs/makefigures/illustrative_traj.py
@@ -5,42 +5,53 @@ import case_config
 
 plt.style.use("lm.mplstyle")
 
-#DATA_ROOT = "/home/chris/Dropbox/readdy_learn/reaction_learn_data"
-DATA_ROOT = "/srv/public/chrisfr/workspace/data/reaction_learn_data"
+DATA_ROOT = "/home/chris/Dropbox/readdy_learn/reaction_learn_data"
+# DATA_ROOT = "/srv/public/chrisfr/workspace/data/reaction_learn_data"
 
 traj = case_config.get_traj_from_file(os.path.join(DATA_ROOT, "gillespie_trajs_init_1.h5"), 20000, 0)
 
-#which = "mrna"
-#which = "protein"
-which = "all"
 
-if which=="mrna":
-    species = np.array([1, 4, 7])
-    ylim = (-0.05, 0.85)
-elif which=="protein":
-    species = np.array([2, 5, 8])
-    ylim = (-0.05, 0.85)
-elif which=="all":
-    species = np.array([1, 2, 4, 5, 7, 8])
-    ylim = (-0.05, 0.85)
-else:
-    raise ValueError("blub")
+def plot_species(species):
+    plot_format = ["-", "--"] * 6
+    for ix, s in enumerate(species):
+        name = case_config.SPECIES_TEX[s]
+        plt.plot(t[::stride], traj[0][:, s][::stride], plot_format[ix], label=r"${}$".format(name),
+                 #color='C{}'.format(ix)
+                 color="black"
+                 )
+    plt.legend()
 
-filename = "illustrative_traj_init_1_"+which+".pdf"
 
-plot_format = ["-"] * 6
-#plot_format = ["-", "--", ":","-", "--", ":"]
-
-t = np.arange(len(traj[0]))*case_config.TIMESTEP
+t = np.arange(len(traj[0])) * case_config.TIMESTEP
 stride = 1
-for ix, s in enumerate(species):
-    name = case_config.SPECIES_TEX[s]
-    plt.plot(t[::stride], traj[0][:,s][::stride], plot_format[ix], label=r"${}$".format(name), color='C{}'.format(ix))
-plt.legend(loc="lower right")
-plt.ylim(ylim)
-plt.xlabel('Time in a.u.')
-plt.ylabel('Concentration in a.u.')
-plt.gcf().tight_layout()
 
-plt.savefig(filename)
+# width = 7.1 # double columns
+width = 7.1 / 2.  # single column
+n_rows = 3
+height = n_rows * 1.5
+n_cols = 1
+
+fig, axarr = plt.subplots(n_rows, n_cols, figsize=(width, height), sharex=True)
+axlist = axarr.flatten()
+
+plt.sca(axlist[0])
+plot_species(np.array([2, 1]))
+#plt.ylabel('Concentration in a.u.')
+
+plt.sca(axlist[1])
+plot_species(np.array([5, 4]))
+#plt.ylabel('Concentration in a.u.')
+
+plt.sca(axlist[2])
+plot_species(np.array([8, 7]))
+plt.xlabel('Time in a.u.')
+#plt.ylabel('Concentration in a.u.')
+
+fig.tight_layout()
+fig.subplots_adjust(hspace=0)
+plt.setp([a.get_xticklabels() for a in fig.axes[:-1]], visible=False)
+
+plt.savefig("./illustrative-traj.pdf", transparent=True)
 plt.show()
+plt.clf()
+plt.close(fig)
