d2ac80d1ea5d97ab56a3ac1cf8c269907332309f
Moritz Hoffmann <clonker@gmail.com>
2018-10-12 16:04:39 +0200

    jcp submission state

--- docs/makefigures/convert.sh
+++ docs/makefigures/convert.sh
@@ -1,5 +1,5 @@
-convert -flatten -density 300 -units PixelsPerInch sparsity_pattern.png -resize 1011 sparsity_pattern.tiff
-convert -flatten -density 300 -units PixelsPerInch trajectories_lsq.png -resize 1011 trajectories_lsq.tiff
-convert -flatten -density 300 -units PixelsPerInch case1_cutoff.png -resize 1645.74 case1_cutoff.tiff
-convert -flatten -density 300 -units PixelsPerInch case2.png -resize 1011 case2.tiff
-convert -flatten -density 300 -units PixelsPerInch case3.png -resize 1011 case3.tiff
+convert -flatten -density 300 -units PixelsPerInch sparsity_pattern.png -resize 1011 "3.tiff"
+convert -flatten -density 300 -units PixelsPerInch trajectories_lsq.png -resize 1011 "2.tiff"
+convert -flatten -density 300 -units PixelsPerInch case1_cutoff.png -resize 1645.74 "6.tiff"
+convert -flatten -density 300 -units PixelsPerInch case2.png -resize 1011 "4.tiff"
+convert -flatten -density 300 -units PixelsPerInch case3.png -resize 1011 "5.tiff"
