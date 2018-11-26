f9d48e48f40af22b63af1b2b9fb3d576d9800486
Moritz Hoffmann <clonker@gmail.com>
2018-11-26 17:37:32 +0100

    wild case of copy paste

--- readdy_learn/example/predator_prey.py
+++ readdy_learn/example/predator_prey.py
@@ -1,17 +1,9 @@
 import numpy as _np
 import pynumtools.kmc as _kmc
+
 import readdy_learn.analyze.basis as _basis
-import matplotlib.pyplot as plt
-from pathos.multiprocessing import Pool as _Pool
-import readdy_learn.analyze.progress as _pr
-import readdy_learn.analyze.interface as _interface
-import readdy_learn.analyze.analyze as _ana
-from pynumtools.finite_differences import fd_coefficients as _fd_coeffs
-from pynumtools.util import sliding_window as _sliding_window
 
-from readdy_learn.analyze.interface import ReactionAnalysisObject
-
-DEFAULT_DESIRED_RATES = _np.array([
+RATES = _np.array([
     1.,  # X + X -> 0
     1.,  # Y + Y -> 0
     1.,  # X -> X + X
@@ -19,142 +11,133 @@ DEFAULT_DESIRED_RATES = _np.array([
     1.,  # Y -> 0
 ])
 
+SPECIES_NAMES = ["X", "Y"]
+INITIAL_STATES = _np.array([
+    [10, 3],
+])
+TIMESTEP = 1e-3
 
-class PredatorPrey(_interface.AnalysisObjectGenerator):
 
-    def __init__(self):
-        self.n_species = 2
-        self.species_names = ["X", "Y"]
-        initial_states = [
-            [10, 3],
-        ]
-        self._initial_states = [_np.array([arr]) for arr in initial_states]
-        self.timestep = 1e-3
-        self.ld_derivative_config = {
-            'ld_derivative_atol': 1e-4,
-            'ld_derivative_rtol': None,
-            'ld_derivative_linalg_solver_maxit': 100000,
-            'ld_derivative_alpha': 1e-1,
-            'ld_derivative_solver': 'spsolve',
-            'ld_derivative_linalg_solver_tol': 1e-10,
-            'ld_derivative_use_preconditioner': False
-        }
-        self._target_time = 3.
-        self._noise_variance = 1e-2
+def bfc():
+    result = _basis.BasisFunctionConfiguration(len(SPECIES_NAMES))
 
-    def set_up_system(self, init_state):
-        sys = _kmc.ReactionDiffusionSystem(diffusivity=self.n_species * [[[0.]]], n_species=self.n_species, n_boxes=1,
-                                           init_state=init_state, species_names=self.species_names)
-        # usual stuff A
-        sys.add_fusion("X", "X", None, _np.array([self.desired_rates[0]]))
-        sys.add_fusion("Y", "Y", None, _np.array([self.desired_rates[1]]))
-        sys.add_fission("X", "X", "X", _np.array([self.desired_rates[2]]))
-        sys.add_double_conversion(["X", "Y"], ["Y", "Y"], _np.array([self.desired_rates[3]]))
-        sys.add_decay("Y", _np.array([self.desired_rates[4]]))
-        return sys
+    result.add_fusion(0, 0, None)
+    result.add_fusion(1, 1, None)
+    result.add_fission(0, 0, 0)
+    result.add_double_conversion([0, 1], [1, 1])
+    result.add_decay(1)
 
-    def generate_analysis_object(self, fname_prefix=None, fname_postfix=None) -> ReactionAnalysisObject:
-        if fname_prefix is None:
-            fname_prefix = "regulation_network"
-        if fname_postfix is None:
-            fname_postfix = ""
-        analysis = _ana.ReactionAnalysis(self.get_bfc(), self.desired_rates, self.initial_states, self.set_up_system,
-                                         fname_prefix=fname_prefix, fname_postfix=fname_postfix,
-                                         n_species=self.n_species, timestep=self.timestep,
-                                         ld_derivative_config=self.ld_derivative_config, recompute_traj=False,
-                                         species_names=self.species_names)
-        return analysis
+    return result
 
-    @property
-    def initial_states(self):
-        return self._initial_states
 
-    @property
-    def target_time(self):
-        return self._target_time
+def derivative(times, counts):
+    dcounts_dt = _np.empty_like(counts)
+    for sp in range(len(SPECIES_NAMES)):
+        x = counts[:, sp]
 
-    @target_time.setter
-    def target_time(self, value):
-        self._target_time = value
+        indices = 1 + _np.where(x[:-1] != x[1:])[0]
+        indices = _np.insert(indices, 0, 0)
 
-    @property
-    def noise_variance(self):
-        return self._noise_variance
+        interpolated = _np.interp(times, times[indices], x[indices])
+        interpolated = _np.gradient(interpolated) / TIMESTEP
 
-    @noise_variance.setter
-    def noise_variance(self, value):
-        self._noise_variance = value
+        dcounts_dt[:, sp] = interpolated
+    return dcounts_dt
 
-    def compute_gradient_derivatives(self, analysis: ReactionAnalysisObject, persist: bool = True, wwidth=None):
-        for t in range(len(self.initial_states)):
-            traj = analysis.get_traj(t)
-            # for sp in [0, 3, 6]:
-            #    dx = _np.zeros_like(traj.counts[:, sp])
-            #    print("species {} dx.shape {}".format(sp, dx.shape))
-            #    traj.separate_derivs[sp] = dx
-            times = traj.times
 
-            for sp in [0, 1]:
-                x = traj.counts[:, sp]
-                dt = traj.time_step
+def generate_lma(initial_state: int, target_time: float):
+    import readdy_learn.analyze.generate as generate
+    _, counts = generate.generate_continuous_counts(RATES, INITIAL_STATES[initial_state],
+                                                    bfc(), TIMESTEP, target_time / TIMESTEP,
+                                                    noise_variance=0, n_realizations=1)
+    times = _np.linspace(0, counts.shape[0] * TIMESTEP, endpoint=False, num=counts.shape[0])
 
-                indices = 1 + _np.where(x[:-1] != x[1:])[0]
-                indices = _np.insert(indices, 0, 0)
+    for sp in range(len(SPECIES_NAMES)):
+        x = counts[:, sp]
+        indices = 1 + _np.where(x[:-1] != x[1:])[0]
+        indices = _np.insert(indices, 0, 0)
+        interpolated = _np.interp(times, times[indices], x[indices])
+        counts[:, sp] = interpolated
 
-                if wwidth is not None:
-                    if len(_np.atleast_1d(indices.squeeze())) == 1:
-                        indices = _np.arange(2 * wwidth + 1, dtype=int)
+    dcounts_dt = derivative(times, counts)
 
-                    unique_times, unique_counts = times[indices].squeeze(), x[indices].squeeze()
-                    unique_deriv = _np.empty_like(unique_counts)
+    return counts, dcounts_dt
 
-                    for ix, (wx, wy) in enumerate(
-                            zip(_sliding_window(_np.atleast_1d(unique_times), width=wwidth, fixed_width=False),
-                                _sliding_window(_np.atleast_1d(unique_counts), width=wwidth, fixed_width=False))):
-                        x = unique_times[ix]
-                        coeff = _fd_coeffs(x, wx, k=1)
-                        unique_deriv[ix] = coeff.dot(wy)
 
-                    interpolated = _np.interp(times, unique_times, unique_deriv)
-                    traj.separate_derivs[sp] = interpolated
-                else:
-                    interpolated = _np.interp(times, times[indices], x[indices])
-                    interpolated = _np.gradient(interpolated) / dt
+def generate_kmc(initial_state: int, target_time: float, n_realizations: int, njobs: int):
+    import readdy_learn.analyze.generate as generate
+    sys = _kmc.ReactionDiffusionSystem(diffusivity=len(SPECIES_NAMES) * [[[0.]]],
+                                       n_species=len(SPECIES_NAMES), n_boxes=1,
+                                       init_state=INITIAL_STATES[initial_state],
+                                       species_names=SPECIES_NAMES)
+    sys.add_fusion("X", "X", None, _np.array([RATES[0]]))
+    sys.add_fusion("Y", "Y", None, _np.array([RATES[1]]))
+    sys.add_fission("X", "X", "X", _np.array([RATES[2]]))
+    sys.add_double_conversion(["X", "Y"], ["Y", "Y"], _np.array([RATES[3]]))
+    sys.add_decay("Y", _np.array([RATES[4]]))
 
-                    traj.separate_derivs[sp] = interpolated
+    assert initial_state < len(INITIAL_STATES)
+    _, counts = generate.generate_averaged_kmc_counts(lambda: sys, target_time, TIMESTEP,
+                                                      n_realizations=n_realizations, njobs=njobs)
+    times = _np.linspace(0, counts.shape[0] * TIMESTEP, endpoint=False, num=counts.shape[0])
 
-            if persist:
-                traj.persist()
+    for sp in range(len(SPECIES_NAMES)):
+        x = counts[:, sp]
+        indices = 1 + _np.where(x[:-1] != x[1:])[0]
+        indices = _np.insert(indices, 0, 0)
+        interpolated = _np.interp(times, times[indices], x[indices])
+        counts[:, sp] = interpolated
 
-    def interpolate_counts(self, analysis: _ana.ReactionAnalysis, persist: bool = True):
-        for t in range(len(self.initial_states)):
-            traj = analysis.get_traj(t)
-            times = traj.times
+    dcounts_dt = derivative(times, counts)
 
-            for sp in [0, 1]:
-                x = traj.counts[:, sp]
+    return counts, dcounts_dt
 
-                indices = 1 + _np.where(x[:-1] != x[1:])[0]
-                indices = _np.insert(indices, 0, 0)
 
-                interpolated = _np.interp(times, times[indices], x[indices])
+def solve(counts, dcounts_dt, alpha, l1_ratio):
+    import readdy_learn.analyze.estimator as rlas
+    import readdy_learn.analyze.tools as tools
 
-                traj.counts[:, sp] = interpolated
+    tolerances_to_try = [1e-16, 1e-15, 1e-14, 1e-13, 1e-12, 1e-11, 1e-10, 1e-9, 1e-8]
 
-            if persist:
-                traj.persist()
+    if isinstance(counts, (list, tuple)):
+        assert isinstance(dcounts_dt, (list, tuple))
+        counts = _np.concatenate(counts, axis=0).squeeze()
+        dcounts_dt = _np.concatenate(dcounts_dt, axis=0).squeeze()
+    else:
+        assert isinstance(counts, _np.ndarray) and isinstance(dcounts_dt, _np.ndarray)
 
-    @property
-    def desired_rates(self):
-        return DEFAULT_DESIRED_RATES
+    traj = tools.Trajectory(counts, time_step=TIMESTEP)
+    traj.dcounts_dt = dcounts_dt
 
-    def get_bfc(self):
-        bfc = _basis.BasisFunctionConfiguration(self.n_species)
+    estimator = None
+    for tol in tolerances_to_try:
+        print("Trying tolerance {}".format(tol))
+        estimator = rlas.ReaDDyElasticNetEstimator([traj], bfc(), alpha=alpha, l1_ratio=l1_ratio,
+                                                   maxiter=30000, method='SLSQP', verbose=True, approx_jac=False,
+                                                   options={'ftol': tol}, rescale=False,
+                                                   init_xi=_np.zeros_like(RATES),
+                                                   constrained=True)
 
-        bfc.add_fusion(0, 0, None)
-        bfc.add_fusion(1, 1, None)
-        bfc.add_fission(0, 0, 0)
-        bfc.add_double_conversion([0, 1], [1, 1])
-        bfc.add_decay(1)
+        estimator.fit(None)
+        if estimator.success_:
+            return estimator.coefficients_
+    if estimator is not None:
+        raise ValueError('*_*: {}, {}'.format(estimator.result_.status, estimator.result_.message))
+    else:
+        raise ValueError('-_-')
 
-        return bfc
+
+def cv(counts, dcounts_dt, alphas=(1.,), l1_ratios=(1.,), n_splits=5, njobs=1):
+    import readdy_learn.analyze.tools as tools
+    import readdy_learn.analyze.cross_validation as cross_validation
+
+    traj = tools.Trajectory(counts, time_step=TIMESTEP)
+    traj.dcounts_dt = dcounts_dt
+    cv = cross_validation.CrossValidation([traj], bfc())
+    cv.splitter = 'kfold'
+    cv.n_splits = n_splits
+    cv.njobs = njobs
+    cv.show_progress = True
+    cv_result = cv.cross_validate(alphas, l1_ratios, realizations=1)
+    result = {"cv_result": cv_result}
+    return result
